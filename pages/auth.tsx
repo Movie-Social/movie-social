f8cede7943ada82ba1c43fe9f4e047304eea1ee7
Kendall McGree <mcgreekendall@gmail.com>
2023-06-16 14:59:26 -0500

    pass new props from Auth page to Input component for Username and new input fields

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,6 +1,10 @@
 import Input from "@/components/Input";
+import { useState } from "react";
 
 const Auth = () => {
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   return (
     <main className="relative h-full w-full bg-[url('/images/socialHero.png')] bg-no-repeat bg-center bg-fixed bg-cover">
       <section className="bg-black w-full h-full bg-opacity-50 ">
@@ -11,8 +15,54 @@ const Auth = () => {
           <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 w-3/5 max-w-md rounded-md w-full">
             <h2 className="text-white text-3xl mb-8 font-semibold">Sign In</h2>
             <section className="flex flex-col gap-4">
-              <Input />
+              <Input
+                label="Username"
+                onChange={(e: any) => setUsername(e.target.value)}
+                id="username"
+                value={username}
+              />
+              <Input
+                label="Email"
+                onChange={(e: any) => setEmail(e.target.value)}
+                id="email"
+                type="email"
+                value={email}
+              />
+              <Input
+                label="Password"
+                onChange={(e: any) => setPassword(e.target.value)}
+                id="password"
+                type="password"
+                value={password}
+              />
             </section>
+            <button
+              className="
+            bg-yellow-600
+            py-3
+            text-white
+            rounded-md
+            w-full
+            mt-10
+            hover:bg-yellow-700
+            transition
+            "
+            >
+              Login
+            </button>
+            <p
+              className="text-neutral-500
+            mt-12"
+            >
+              First time using Movie Social?{" "}
+              <span
+                className="text-white
+              ml-1
+              hover:underline cursor-pointer"
+              >
+                Create an account
+              </span>
+            </p>
           </div>
         </div>
       </section>
