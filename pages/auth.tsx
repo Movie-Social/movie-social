1683a5db533c248a0221a0958aec45c5ea17df9d
Kendall McGree <mcgreekendall@gmail.com>
2023-06-16 13:37:21 -0500

    add new images to public folder and create new Auth page with initial styling

---
+++ pages/auth.tsx
@@ -0,0 +1,8 @@
+const Auth = () => {
+  return (
+    <section className="relative h-full w-full bg-[url('/images/socialHero.png')] bg-no-repeat bg-center bg-fixed bg-cover ">
+      <h1>Auth</h1>
+    </section>
+  );
+};
+export default Auth;
