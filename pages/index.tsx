02bcec582cd6480ff143ec3c9318e2e013a55b18
Kendall McGree <mcgreekendall@gmail.com>
2023-06-18 15:08:18 -0500

    add redirect to homepage for people who are not in a current, logged on session

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,29 @@
+import { NextPageContext } from "next";
+import { getSession, signOut } from "next-auth/react";
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth",
+        permanent: false,
+      },
+    };
+  }
+  return {
+    props: {},
+  };
+}
 const Home = () => {
   return (
-    <section>
+    <main>
       <h1 className="text-2xl text-yellow-500">Movie Social</h1>
-    </section>
+      <button className="h-10 w-full bg-white" onClick={() => signOut()}>
+        Logout!
+      </button>
+    </main>
   );
 };
 export default Home;
