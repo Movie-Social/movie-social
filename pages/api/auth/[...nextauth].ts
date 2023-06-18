08eb5be49a61fe51389057abb106b762e9ecd208
Kendall McGree <mcgreekendall@gmail.com>
2023-06-18 12:20:37 -0500

    add google and github keys to .env file, install and import providers into [...nextauth].ts file

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,11 +1,20 @@
 //middleware
 import NextAuth from "next-auth";
 import Credentials from "next-auth/providers/credentials";
-import prismadb from "@/lib/prismadb";
 import { compare } from "bcrypt";
-
+import prismadb from "@/lib/prismadb";
+import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
 export default NextAuth({
   providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID || "",
+      clientSecret: process.env.GITUHUB_SECRET || "",
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+    }),
     Credentials({
       id: "credentials",
       name: "Credentials",
