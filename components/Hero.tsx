c592d4bb5bc64b1003204ff5bd75fa288e1f1f1b
Kendall McGree <mcgreekendall@gmail.com>
2023-06-19 09:57:03 -0500

    add images for hero section and create Hero component with initial styling

---
+++ components/Hero.tsx
@@ -0,0 +1,9 @@
+const Hero = () => {
+  return (
+    <main className="relative h-[56.25vw] lg:h-2/4 opacity-50">
+      <img className="w-full" src="/images/hero/IMG_8084.jpg" />
+    </main>
+  );
+};
+
+export default Hero;
