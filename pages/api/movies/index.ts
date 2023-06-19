c596c5efad48d7a658b1bfa77674bf39d31ec9d2
Kendall McGree <mcgreekendall@gmail.com>
2023-06-19 16:35:53 -0500

    add access error handling to new movie endpoint

--- pages/api/movies/index.ts
+++ pages/api/movies/index.ts
@@ -1,13 +1,18 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import prismadb from "@/lib/prismadb";
 import serverAuth from "@/lib/serverAuth";
+import prismadb from "@/lib/prismadb";
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  if (req.method !== "GET") {
+    return res.status(405).end();
+  }
   try {
     await serverAuth(req);
     const movies = await prismadb.movie.findMany();
+    // console.log(movies, "movies");
     return res.status(200).json(movies);
   } catch (error) {
     console.log(error);
