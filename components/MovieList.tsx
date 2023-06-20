eba72cc9364276dc482392ecde2a3074aa5e6765
Kendall McGree <mcgreekendall@gmail.com>
2023-06-20 16:42:24 -0500

    add feature that moves poster when hover in the MovieCard component

--- components/MovieList.tsx
+++ components/MovieList.tsx
@@ -9,7 +9,7 @@ const MovieList: React.FC<MovieListProps> = ({ data, title }) => {
     return null;
   }
   return (
-    <main className="px-4 md:px-12 mt-4 space-y-8">
+    <main className="px-4 md:px-12 mt-2 space-y-4">
       <section>
         <p className="text-white text-md md:text-xl lg:text-2xl font-semibold mb-4">
           {title}
