4f526ab53877c1870c4816e215626f3e571d0e45
Kendall McGree <mcgreekendall@gmail.com>
2023-06-20 16:42:24 -0500

    add feature that moves poster when hover in the MovieCard component

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -4,8 +4,58 @@ interface MovieCardProps {
 
 const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
   return (
-    <main className="grouo bg-zinc-900 col-span relative h-[12vw]">
-      <img src={data.poster} alt="Movie poster" />
+    <main className="group bg-zinc-900 col-span relative h-[12vw]">
+      <img
+        className="
+      cursor-pointer
+      object-fill
+      transition
+      duration
+      shadow-xl
+      rounded-md
+      group-hover:opacity-90
+      sm:group-hover:opacity-0
+      delay-300
+      w-full
+      h-[30vw]
+      "
+        src={data.poster}
+        alt="Movie poster"
+      />
+      <section
+        className="
+      opacity-0
+      absolute
+      top-0
+      transition
+      duration-200
+      z-10
+      invisible
+      sm:visible
+      delay-300
+      w-full
+      scale-0
+      group-hover:scale-110
+      group-hover:-translate-y-[6vw]
+      group-hover:-translate-x-[2vw]
+      group-hover:opacity-100
+      "
+      >
+        <img
+          className="
+        cursor-pointer
+        object-fill
+        transition
+        duration
+        shadow-xl
+        rounded-t-md
+        w-full
+        h-[30vw]
+        "
+          src={data.poster}
+          alt="thumbnail"
+        />
+      </section>
     </main>
   );
 };
