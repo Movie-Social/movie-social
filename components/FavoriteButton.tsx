55bdacf2a71a108f17be10b062d2110f102ae3d7
Kendall McGree <mcgreekendall@gmail.com>
2023-06-22 17:20:23 -0500

    add styling and icons to FavoriteButton component

--- components/FavoriteButton.tsx
+++ components/FavoriteButton.tsx
@@ -2,13 +2,33 @@ import axios from "axios";
 import React, { useCallback, useMemo } from "react";
 import useCurrentUser from "@/hooks/useCurrentUser";
 import useFavorites from "@/hooks/useFavorites";
-
+import { MdFavorite } from "react-icons/md";
 interface FavoriteButtonProps {
   movieId: string;
 }
 
 const FavoriteButton: React.FC<FavoriteButtonProps> = ({ movieId }) => {
-  return <main></main>;
+  return (
+    <main
+      className="cursor-pointer
+  group/item
+  w-6
+  h-6
+  lg:w-10
+  lg:h-10
+  border-white
+  border-2
+  rounded-full
+  flex
+  justify-center
+  items-center
+  transition
+  hover:border-neutral-300
+  "
+    >
+      <MdFavorite className="text-white self-center" size={25} />
+    </main>
+  );
 };
 
 export default FavoriteButton;
