928ffacf14c1dc9a390aae30fdd57a23a3efd4f3
Kendall McGree <mcgreekendall@gmail.com>
2023-06-23 16:30:51 -0500

    add catch block to fetcher lib

--- lib/fetcher.ts
+++ lib/fetcher.ts
@@ -1,3 +1,11 @@
 import axios from "axios";
-const fetcher = (url: string) => axios.get(url).then((res) => res.data);
+const fetcher = (url: string) =>
+  axios
+    .get(url)
+    .then((res) => res.data)
+    .catch((error) => {
+      // Handle error
+      console.error(`Error fetching data from ${url}`, error);
+      throw error;
+    });
 export default fetcher;
