9c08f2663a43c29599501ec7e24c41ec88655ec5
Kendall McGree <mcgreekendall@gmail.com>
2023-06-24 10:26:44 -0500

    update imports in root index file for Modal functionality

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import MovieList from "@/components/MovieList";
 import Navbar from "@/components/Navbar";
 import useCurrentUser from "@/hooks/useCurrentUser";
 import useFavorites from "@/hooks/useFavorites";
+import useInfoModal from "@/hooks/useInfoModal";
 import useMovieList from "@/hooks/useMovieList";
 import { NextPageContext } from "next";
 import { getSession, signOut } from "next-auth/react";
@@ -28,10 +29,11 @@ const Home = () => {
   // const { data: user } = useCurrentUser();
   const { data: movies = [] } = useMovieList();
   const { data: favorites = [] } = useFavorites();
+  const { isOpen, closeModal } = useInfoModal();
 
   return (
     <main>
-      <InfoModal visible onClose={() => {}} />
+      <InfoModal visible={isOpen} onClose={closeModal} />
       <Navbar />
       <Hero />
       <section className="pb-40">
