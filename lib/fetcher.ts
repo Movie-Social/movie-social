4831a29a132fa647a5102a62c01b4b89bb6fda3b
Kendall McGree <mcgreekendall@gmail.com>
2023-06-25 17:43:19 -0500

    install pino and test in fetcher.ts file, working

--- lib/fetcher.ts
+++ lib/fetcher.ts
@@ -1,11 +1,13 @@
 import axios from "axios";
+import pino from "pino";
+const logger = pino();
 const fetcher = (url: string) =>
   axios
     .get(url)
-    .then((res) => res.data)
+    .then((res) => resdata)
     .catch((error) => {
       // Handle error
-      console.error(`Error fetching data from ${url}`, error);
+      logger.info(`Error fetching data from ${url}`, error);
       throw error;
     });
 export default fetcher;
