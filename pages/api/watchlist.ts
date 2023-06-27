6e4e32f776869fc92814d4c6f6f7a8515277b467
Kendall McGree <mcgreekendall@gmail.com>
2023-06-26 20:09:36 -0500

    add watchlist to prismadb schema, push schema to db, and create endpoint for fetching full watchlist

---
+++ pages/api/watchlist.ts
@@ -0,0 +1,28 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import prismadb from "@/lib/prismadb";
+import serverAuth from "@/lib/serverAuth";
+import logger from "@/lib/logger";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== "GET") {
+    return res.status(405).end();
+  }
+  try {
+    const { currentUser } = await serverAuth(req, res);
+    const watchlist = await prismadb.movie.findMany({
+      where: {
+        id: {
+          in: currentUser?.watchlistIds,
+        },
+      },
+    });
+
+    return res.status(200).json(watchlist);
+  } catch (error: any) {
+    logger.error(error.message);
+    return res.status(400).end();
+  }
+}
