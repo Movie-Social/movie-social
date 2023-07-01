7ac56aa4cb2fb6f7e0f9cab059c93609b22d3ee8
Kendall McGree <mcgreekendall@gmail.com>
2023-06-30 19:34:26 -0500

    add conditional rendering for empty lists

--- pages/myProfile.tsx
+++ pages/myProfile.tsx
@@ -4,6 +4,8 @@ import useFavorites from "@/hooks/useFavorites";
 import useWatchlist from "@/hooks/useWatchlist";
 import useCurrentUser from "@/hooks/useCurrentUser";
 import { BiUserCircle } from "react-icons/bi";
+import { GrAddCircle } from "react-icons/gr";
+
 const MyProfile = () => {
   const { data: currentUser } = useCurrentUser();
   const { data: faves } = useFavorites();
@@ -13,7 +15,7 @@ const MyProfile = () => {
   console.log(watchlist, "watchlist");
   return (
     <main className="flex justify-center text-white">
-      <main className="flex flex-row justify-around w-[70vw] h-full border-2 border-red-500">
+      <main className="flex flex-row justify-around w-[70vw] py-5 border-2 border-red-500">
         <aside className="border-2 border-yellow-500 w-1/5">
           <h2 className="border-l-2 border-yellow-500 mx-2 px-2 text-1xl lg:text-2xl font-bold">
             Profile
@@ -28,8 +30,30 @@ const MyProfile = () => {
         </aside>
 
         <section className="border-2 border-blue-500 w-3/5">
-          <MovieList data={faves} title="My Favorites" />
-          <MovieList data={watchlist} title="My Watchlist" />
+          {faves?.length > 1 ? (
+            <MovieList data={faves} title="My Favorites" />
+          ) : (
+            <div className="flex flex-col justify-center items-center content-center border-red-500 border-2">
+              <GrAddCircle size={50} />
+              <h2>Your Favorites are Empty</h2>
+              <p>Add movies to keep track of your favorites</p>
+              <button className="bg-yellow-500 rounded-sm">
+                Browse Movies
+              </button>
+            </div>
+          )}
+          {watchlist?.length > 1 ? (
+            <MovieList data={watchlist} title="My Watchlist" />
+          ) : (
+            <div className="flex flex-col justify-center items-center content-center border-red-500 border-2">
+              <GrAddCircle size={50} />
+              <h2>Your Watchlist is Empty</h2>
+              <p>Add movies to keep track of what you want to watch</p>
+              <button className="bg-yellow-500 rounded-sm">
+                Browse Movies
+              </button>
+            </div>
+          )}
         </section>
       </main>
     </main>
