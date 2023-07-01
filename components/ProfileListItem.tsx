61d5779b716f8b76bbe6e2f3b9f8c9c1043ebeeb
Kendall McGree <mcgreekendall@gmail.com>
2023-07-01 11:20:00 -0500

    add routing to components that take the user to MovieDetails page

--- components/ProfileListItem.tsx
+++ components/ProfileListItem.tsx
@@ -1,15 +1,28 @@
 import useMovie from "@/hooks/useMovie";
-
+import Image from "next/image";
+import { useRouter } from "next/router";
 interface ListItemProps {
   movieId: string;
 }
 const ProfileListItem: React.FC<ListItemProps> = ({ movieId }) => {
   const { data } = useMovie(movieId as string);
-
+  const router = useRouter();
   return (
-    <main className="">
-      <img src={data?.poster} alt={`${data?.title}'s official data poster`} />
-      <h2>{data?.title}</h2>
+    <main className="w-full h-full">
+      <Image
+        onClick={() => router.push(`/movie/${data?.movieId}`)}
+        className="cursor-pointer"
+        width={150}
+        height={50}
+        src={data?.poster}
+        alt={`${data?.title}'s official movie poster`}
+      />
+      <h2
+        className="cursor-pointer transition hover:text-yellow-300"
+        onClick={() => router.push(`/movie/${data?.movieId}`)}
+      >
+        {data?.title}
+      </h2>
     </main>
   );
 };
