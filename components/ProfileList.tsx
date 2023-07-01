35a6c4c1738ea57c0f484cc6452432f1b08a4ca5
Kendall McGree <mcgreekendall@gmail.com>
2023-07-01 10:36:15 -0500

    add styling and elements to ProfileList wrapper component

--- components/ProfileList.tsx
+++ components/ProfileList.tsx
@@ -1,6 +1,5 @@
 import { isEmpty } from "lodash";
 import ProfileListItem from "./ProfileListItem";
-
 interface ProfileListProps {
   data: Record<string, any>[];
   title: string;
@@ -12,8 +11,15 @@ const ProfileList: React.FC<ProfileListProps> = ({ data, title }) => {
   }
   return (
     <main>
-      <h1>{title}</h1>
-      <div>
+      <div className="flex flex-row justify-between">
+        <h2 className="border-l-2 border-yellow-500 mx-2 px-2 text-1xl lg:text-2xl font-bold">
+          {title}
+        </h2>
+        <h2 className="text-yellow-400 cursor-pointer text-l lg:text-xl font-light">
+          View All
+        </h2>
+      </div>
+      <div className="relative flex justify-around border-blue border-2 my-3">
         {data.map((movie) => {
           return <ProfileListItem key={movie.id} movieId={movie.id} />;
         })}
