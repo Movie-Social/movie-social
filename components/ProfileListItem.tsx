78e5341a62797b3cc1e9641009d912aa30fe2411
Kendall McGree <mcgreekendall@gmail.com>
2023-06-30 22:17:58 -0500

    create ProfileList Item component for future ProfileList wrapper component that will map the items

---
+++ components/ProfileListItem.tsx
@@ -0,0 +1,16 @@
+import useMovie from "@/hooks/useMovie";
+
+interface ListItemProps {
+  movieId: string;
+}
+const ProfileListItem: React.FC<ListItemProps> = ({ movieId }) => {
+  const { data } = useMovie(movieId as string);
+
+  return (
+    <main className="">
+      <img src={data.poster} alt={`${data.title}'s official data poster`} />
+      <h2>{data.title}</h2>
+    </main>
+  );
+};
+export default ProfileListItem;
