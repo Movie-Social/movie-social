3fa782f3e92fbc5d3f142bfd6b31a24a48306e67
Kendall McGree <mcgreekendall@gmail.com>
2023-07-02 16:13:44 -0500

    add initial stylign to ReviewList components

--- components/ReviewListItem.tsx
+++ components/ReviewListItem.tsx
@@ -1,13 +1,26 @@
+import Image from "next/image";
+
 interface ReviewListItemProps {
   data: Record<string, any>;
 }
 
 const ReviewListItem: React.FC<ReviewListItemProps> = ({ data }) => {
-  console.log(data, "<<");
   return (
-    <main>
-      <h2>{data?.title}</h2>
-    </main>
+    <div className="flex justify-center">
+      <main className="border-b-2 border-yellow-300 w-[95%] my-3 p-3">
+        <h2>{data?.title}</h2>
+        <section className="flex flex-row">
+          <Image
+            src={data.poster}
+            alt={`${data.title}'s official 
+          movie poster`}
+            width={130}
+            height={50}
+          />
+          <p className="mx-3">{data.review}</p>
+        </section>
+      </main>
+    </div>
   );
 };
 export default ReviewListItem;
