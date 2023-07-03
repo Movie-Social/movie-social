1579c7aa7a5ef96a27ec952cb623fa6d73074d29
Kendall McGree <mcgreekendall@gmail.com>
2023-07-03 17:12:33 -0500

    add initial styling to new RestfulMovieList component

--- components/RestfulMovieList.tsx
+++ components/RestfulMovieList.tsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 
 const RestfulMovieList = () => {
   const [popularMovies, setPopularMovies] = useState<any>();
-  const [upcomingMovies, setUpcomingMovies] = useState<any>();
+  // const [upcomingMovies, setUpcomingMovies] = useState<any>();
   // const [nowPlayingMovies, setNowPlayingMovies] = useState<any>();
 
   useEffect(() => {
@@ -12,12 +12,10 @@ const RestfulMovieList = () => {
       setPopularMovies(populars.results);
       // const nowPlayings = await tmdbFetcher("now_playing");
       // setNowPlayingMovies(nowPlayings.results);
+      //   const upcomings = await tmdbFetcher("upcoming");
+      //   setUpcomingMovies(upcomings.results);
     };
 
-    const fetchUpcomings = async () => {
-      const upcomings = await tmdbFetcher("upcoming");
-      setUpcomingMovies(upcomings.results);
-    };
     fetchPopulars();
     // fetchUpcomings();
   }, []);
@@ -25,9 +23,21 @@ const RestfulMovieList = () => {
   // console.log(upcomingMovies, "UP");
   // console.log(nowPlayingMovies, "NP");
   return (
-    <main>
-      <h1>test</h1>
+    <main className="px-4 md:px-12 mt-2 space-y-4">
+      {/* <h1>test</h1> */}
       {/* <h1>{}</h1> */}
+      <section>
+        <p className="text-white text-md md:text-xl lg:text-2xl font-semibold mb-4">
+          {/* {title} */}
+        </p>
+        <div className="grid grid-cols-5 gap-2 mb-24 md:mb-48 lg:mb-96">
+          {/* {data
+            .filter((movie) => movie.categories[0] === title)
+            .map((movie) => (
+              <MovieCard key={movie.id} data={movie} />
+            ))} */}
+        </div>
+      </section>
     </main>
   );
 };
