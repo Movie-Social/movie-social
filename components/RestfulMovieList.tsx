b6a78609f1de9476bb1c6f419fed58e8135535eb
Kendall McGree <mcgreekendall@gmail.com>
2023-07-03 18:44:51 -0500

    create RestfulMovieCard component for displaying mapped movies from RestfulMovieList

--- components/RestfulMovieList.tsx
+++ components/RestfulMovieList.tsx
@@ -1,5 +1,6 @@
 import tmdbFetcher from "@/lib/tmdbFetcher";
 import { useEffect, useState } from "react";
+import RestfulMovieCard from "./RestfulMovieCard";
 
 const RestfulMovieList = () => {
   const [popularMovies, setPopularMovies] = useState<any>();
@@ -12,30 +13,31 @@ const RestfulMovieList = () => {
       setPopularMovies(populars.results);
       // const nowPlayings = await tmdbFetcher("now_playing");
       // setNowPlayingMovies(nowPlayings.results);
-      //   const upcomings = await tmdbFetcher("upcoming");
-      //   setUpcomingMovies(upcomings.results);
+      // const upcomings = await tesFetcher();
+      // setUpcomingMovies(upcomings.results);
     };
 
     fetchPopulars();
     // fetchUpcomings();
   }, []);
+  if (!popularMovies) {
+    return null;
+  }
+
   console.log(popularMovies, "POP");
   // console.log(upcomingMovies, "UP");
   // console.log(nowPlayingMovies, "NP");
   return (
     <main className="px-4 md:px-12 mt-2 space-y-4">
-      {/* <h1>test</h1> */}
-      {/* <h1>{}</h1> */}
       <section>
-        <p className="text-white text-md md:text-xl lg:text-2xl font-semibold mb-4">
-          {/* {title} */}
-        </p>
+        <h2 className="text-white text-md md:text-xl lg:text-2xl font-semibold mb-4">
+          Current Top 5
+        </h2>
         <div className="grid grid-cols-5 gap-2 mb-24 md:mb-48 lg:mb-96">
-          {/* {data
-            .filter((movie) => movie.categories[0] === title)
-            .map((movie) => (
-              <MovieCard key={movie.id} data={movie} />
-            ))} */}
+          {popularMovies.map((movie: any) => {
+            console.log(movie, "<<<<");
+            return <RestfulMovieCard key={movie.id} data={movie} />;
+          })}
         </div>
       </section>
     </main>
