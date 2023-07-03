b25f9f8be99c97e4b2b483d3e22e6a25ab6ed7df
Kendall McGree <mcgreekendall@gmail.com>
2023-07-03 17:04:20 -0500

    make the restful fetch call more dynamic

--- components/RestfulMovieList.tsx
+++ components/RestfulMovieList.tsx
@@ -3,15 +3,27 @@ import { useEffect, useState } from "react";
 
 const RestfulMovieList = () => {
   const [popularMovies, setPopularMovies] = useState<any>();
+  const [upcomingMovies, setUpcomingMovies] = useState<any>();
+  // const [nowPlayingMovies, setNowPlayingMovies] = useState<any>();
 
   useEffect(() => {
     const fetchPopulars = async () => {
-      const populars = await tmdbFetcher();
+      const populars = await tmdbFetcher("popular");
       setPopularMovies(populars.results);
+      // const nowPlayings = await tmdbFetcher("now_playing");
+      // setNowPlayingMovies(nowPlayings.results);
+    };
+
+    const fetchUpcomings = async () => {
+      const upcomings = await tmdbFetcher("upcoming");
+      setUpcomingMovies(upcomings.results);
     };
     fetchPopulars();
+    // fetchUpcomings();
   }, []);
-  console.log(popularMovies[0]);
+  console.log(popularMovies, "POP");
+  // console.log(upcomingMovies, "UP");
+  // console.log(nowPlayingMovies, "NP");
   return (
     <main>
       <h1>test</h1>
