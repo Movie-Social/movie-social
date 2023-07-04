9529917210f235571cc4d3bb3bf6f475d52bae0c
Kendall McGree <mcgreekendall@gmail.com>
2023-07-04 09:42:20 -0500

    update api variable with proper naming and new key. since it is now working dynamically i delete the extra fetcher

--- lib/tmdbFetcher.ts
+++ lib/tmdbFetcher.ts
@@ -1,17 +1,9 @@
 import logger from "./logger";
 
-const options = {
-  method: "GET",
-  headers: {
-    accept: "application/json",
-    Authorization: process.env.TMDB || "",
-  },
-};
 const tmdbFetcher = async (category: string) => {
   try {
     const response = await fetch(
-      `https://api.themoviedb.org/3/movie/${category}?language=en-US&page=1`,
-      options
+      `https://api.themoviedb.org/3/movie/${category}?api_key=${process.env.NEXT_PUBLIC_TMDB}`
     );
     return response.json();
   } catch (error) {
