7bf4083cee3480a633a7d29ff26954146e365b4c
Kendall McGree <mcgreekendall@gmail.com>
2023-07-04 17:49:40 -0500

    add slide feature to RestfulMovieLists

--- components/RestfulMovieCard.tsx
+++ components/RestfulMovieCard.tsx
@@ -6,14 +6,18 @@ interface RestfulMovieCardProps {
 const RestfulMovieCard: React.FC<RestfulMovieCardProps> = ({ data }) => {
   const router = useRouter();
   return (
-    <main>
-      <img
-        onClick={() => router.push(`/movie/tmdb/${data?.id}`)}
-        alt={`${data.title}'s official movie poster"`}
-        src={`https://image.tmdb.org/t/p/original/${data?.poster_path}`}
-        fill
-      />
-      <h2>{data?.title}</h2>
+    <main className="flex flex-col items-center justify-center content-center text-center">
+      <div className="flex justify-center">
+        <img
+          className="self-center"
+          onClick={() => router.push(`/movie/tmdb/${data?.id}`)}
+          alt={`${data.title}'s official movie poster"`}
+          src={`https://image.tmdb.org/t/p/original/${data?.poster_path}`}
+          width={200}
+          height={200}
+        />
+      </div>
+      <h2 className="text-white">{data?.title}</h2>
       <h2>{data?.popularity}</h2>
     </main>
   );
