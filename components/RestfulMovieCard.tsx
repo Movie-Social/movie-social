1788482437cdb18881cef82e4d38e83839e1f8df
Kendall McGree <mcgreekendall@gmail.com>
2023-07-04 11:23:27 -0500

    fix bug that would no t find correct route for movie posters

--- components/RestfulMovieCard.tsx
+++ components/RestfulMovieCard.tsx
@@ -1,11 +1,19 @@
+import { useRouter } from "next/router";
 interface RestfulMovieCardProps {
   data: Record<string, any>;
 }
 
 const RestfulMovieCard: React.FC<RestfulMovieCardProps> = ({ data }) => {
-  //   console.log(data, "<<");
+  const router = useRouter();
+  console.log(data.poster_path, "<<");
   return (
     <main>
+      <img
+        onClick={() => router.push("/auth")}
+        alt={`${data.title}'s official movie poster"`}
+        src={`https://image.tmdb.org/t/p/original/${data?.poster_path}`}
+        fill
+      />
       <h2>{data?.title}</h2>
       <h2>{data?.popularity}</h2>
     </main>
