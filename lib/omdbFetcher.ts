cb89ecbbefd0b233b0e9430d95ca09eb6c7ed140
Kendall McGree <mcgreekendall@gmail.com>
2023-07-04 16:58:56 -0500

    change logic to fetch from tmdb first and then use that data to fetch from omdb in order to have all data needed

--- lib/omdbFetcher.ts
+++ lib/omdbFetcher.ts
@@ -2,7 +2,16 @@ import logger from "@/lib/logger";
 
 const omdbFetcher = async (title: any) => {
   try {
-    const fetchString = title.split(" ").join("+");
+    if (!title) {
+      return null;
+    }
+    let fetchString;
+    if (title.split("").includes(":")) {
+      fetchString = title.split(": ").join("+").split(" ").join("+");
+    } else {
+      fetchString = title.split(" ").join("+");
+    }
+
     const response = await fetch(
       `https://www.omdbapi.com/?t=${fetchString}&apikey=${process.env.NEXT_PUBLIC_OMDB}`
     );
