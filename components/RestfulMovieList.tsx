cce3e634f98f082f1996c1d03a7ae13d7abfbeb7
Kendall McGree <mcgreekendall@gmail.com>
2023-07-04 10:29:08 -0500

    fetch data for other TMDB lists

--- components/RestfulMovieList.tsx
+++ components/RestfulMovieList.tsx
@@ -1,33 +1,40 @@
 import tmdbFetcher from "@/lib/tmdbFetcher";
 import { useEffect, useState } from "react";
 import RestfulMovieCard from "./RestfulMovieCard";
-import upcomingFetcher from "@/lib/upcomingFetcher";
 
 const RestfulMovieList = () => {
-  const [popularMovies, setPopularMovies] = useState<any>();
-  const [upcomingMovies, setUpcomingMovies] = useState<any>();
-  // const [nowPlayingMovies, setNowPlayingMovies] = useState<any>();
+  const [popularMovies, setPopularMovies] = useState([]);
+  const [upcomingMovies, setUpcomingMovies] = useState([]);
+  const [nowPlayingMovies, setNowPlayingMovies] = useState([]);
+  const [topRatedMovies, setTopRatedMovies] = useState([]);
 
   useEffect(() => {
-    const fetchPopulars = async () => {
+    const fetchTMDBLists = async () => {
       const populars = await tmdbFetcher("popular");
       setPopularMovies(populars.results);
-      // const nowPlayings = await tmdbFetcher("now_playing");
-      // setNowPlayingMovies(nowPlayings.results);
-      const upcomings = await upcomingFetcher();
-      setUpcomingMovies(upcomings);
+      const nowPlayings = await tmdbFetcher("now_playing");
+      setNowPlayingMovies(nowPlayings.results);
+      const upcomings = await tmdbFetcher("upcoming");
+      setUpcomingMovies(upcomings.results);
+      const tops = await tmdbFetcher("top_rated");
+      setTopRatedMovies(tops.results);
     };
 
-    fetchPopulars();
-    // fetchUpcomings();
+    fetchTMDBLists();
   }, []);
-  if (!popularMovies) {
+  if (
+    !popularMovies ||
+    !upcomingMovies ||
+    !topRatedMovies ||
+    !nowPlayingMovies
+  ) {
     return null;
   }
 
-  console.log(popularMovies, "POP");
-  console.log(upcomingMovies, "UP");
-  // console.log(nowPlayingMovies, "NP");
+  console.log(popularMovies[0]?.title, "POP");
+  console.log(nowPlayingMovies[0]?.title, "NP");
+  console.log(upcomingMovies[0]?.title, "UP");
+  console.log(topRatedMovies[0]?.title, "TOP");
   return (
     <main className="px-4 md:px-12 mt-2 space-y-4">
       <section>
