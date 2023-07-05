2ff4f4b42141dda8a568a86d5d01c42d77a1419c
Kendall McGree <mcgreekendall@gmail.com>
2023-07-05 18:01:27 -0500

    add initial logic for star rating in Review component

--- components/ReviewForm.tsx
+++ components/ReviewForm.tsx
@@ -1,14 +1,39 @@
 import useCurrentUser from "@/hooks/useCurrentUser";
+import { useMemo, useState } from "react";
 import { BiUserCircle } from "react-icons/bi";
+import { BsFillStarFill, BsStar } from "react-icons/bs";
 
-const Reviewform = () => {
+interface ReviewProps {
+  count: number;
+  onRating: Function;
+  rating: number;
+}
+
+const Reviewform: React.FC<ReviewProps> = ({ count, onRating, rating }) => {
   const currentUser = useCurrentUser();
+  //   const [rating, setRating] = useState(0);
+  const starRating = useMemo(() => {
+    return Array(count)
+      .fill(0)
+      .map((_, i) => i + 1)
+      .map((idx) => {
+        <BsFillStarFill key={idx} size={30} onClick={() => onRating(idx)} />;
+      });
+  }, [count, rating]);
   return (
-    <main className="border-2 border-red-300">
-      <div className="flex flex-row">
-        <BiUserCircle size={30} />
-        {currentUser.data.name}
-      </div>
+    <main className="border-2 border-red-300 relative">
+      <section className="flex flex-row justify-between">
+        <div className="flex flex-row">
+          <BiUserCircle size={30} />
+          {currentUser.data.name}
+        </div>
+        <div className="flex flex-row">
+          {/* <BsFillStarFill className="cursor-pointer" size={30} />
+          <BsFillStarFill size={30} />
+          <BsFillStarFill size={30} />
+          <BsFillStarFill size={30} /> */}
+        </div>
+      </section>
       <div className="stars"></div>
     </main>
   );
