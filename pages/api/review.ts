9eb69941f6de97d3be31c5cecc1065e4ec4e2059
Kendall McGree <mcgreekendall@gmail.com>
2023-07-06 17:42:49 -0500

    add reviews to schema and create a page for posting reviews to a users collection

---
+++ pages/api/review.ts
@@ -0,0 +1,44 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import prismadb from "@/lib/prismadb";
+import serverAuth from "@/lib/serverAuth";
+import logger from "@/lib/logger";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.body !== "POST") {
+    return res.status(405).end();
+  }
+  try {
+    const { currentUser } = await serverAuth(req, res);
+
+    const { movieId } = req.body;
+    const existingMovie = await prismadb.movie.findUnique({
+      where: {
+        id: movieId,
+      },
+    });
+
+    if (!existingMovie) {
+      logger.info("No movie exists");
+      throw new Error("Invalid ID");
+    }
+
+    const user = await prismadb.user.update({
+      where: {
+        email: currentUser.email || "",
+      },
+      data: {
+        reviewIds: {
+          push: movieId,
+        },
+      },
+    });
+
+    return res.status(200).json(user);
+  } catch (error: any) {
+    logger.error(error);
+    return res.status(400).end();
+  }
+}
