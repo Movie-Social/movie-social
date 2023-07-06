01b2691bc9cfdf315594d6633f9f2e226d526442
Kendall McGree <mcgreekendall@gmail.com>
2023-07-06 18:13:29 -0500

    add logic to SubmitReviewButton

--- components/SubmitReviewButton.tsx
+++ components/SubmitReviewButton.tsx
@@ -1,7 +1,49 @@
-const SubmitReviewButton = () => {
+import useCurrentUser from "@/hooks/useCurrentUser";
+import useReviews from "@/hooks/useReviews";
+import axios from "axios";
+import { useCallback } from "react";
+
+interface SubmitReviewButtonProps {
+  movieId: string;
+}
+const SubmitReviewButton: React.FC<SubmitReviewButtonProps> = ({ movieId }) => {
+  const { mutate: mutateReviews } = useReviews();
+  const { data: currentUser, mutate } = useCurrentUser();
+
+  const addReview = useCallback(async () => {
+    const response = await axios.post("/api/review", { movieId });
+    const updatedReviewIds = response?.data?.reviewIds;
+
+    mutate({
+      ...currentUser,
+      reviewIds: updatedReviewIds,
+    });
+
+    mutateReviews();
+    console.log("user", currentUser);
+    console.log("user reviews", currentUser?.reviews);
+  }, [movieId, currentUser, mutate, mutateReviews]);
   return (
-    <main>
-      <button>Submit</button>
+    <main
+      onClick={addReview}
+      className="cursor-pointer
+      m-10
+group/item
+w-6
+h-6
+lg:w-8
+lg:h-8
+border-white
+border-2
+rounded-full
+flex
+justify-center
+items-center
+transition
+hover:border-neutral-300
+"
+    >
+      Submit button
     </main>
   );
 };
