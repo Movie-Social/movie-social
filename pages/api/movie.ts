4093570fe456fcedf3a8bb8a28502bb4bd676c63
Kendall McGree <mcgreekendall@gmail.com>
2023-07-07 17:59:04 -0500

    create endpoint for adding fetched movies to my own db when click by user

---
+++ pages/api/movie.ts
@@ -0,0 +1,42 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import prismadb from "@/lib/prismadb";
+import serverAuth from "@/lib/serverAuth";
+import logger from "@/lib/logger";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== "POST") {
+    return res.status(405).end();
+  }
+  try {
+    // const { currentUser } = await serverAuth(req, res);
+
+    const { title, score, poster, categories, details } = req.body;
+
+    const existingMovie = await prismadb.movie.findFirstOrThrow({
+      where: {
+        title: title,
+      },
+    });
+
+    if (!existingMovie) {
+      // logger.info
+      const newMovie = await prismadb.movie.create({
+        data: {
+          title,
+          score,
+          poster,
+          categories,
+          details,
+        },
+      });
+
+      return res.status(200).json(newMovie);
+    }
+  } catch (error: any) {
+    logger.error(error.message);
+    return res.status(400).end();
+  }
+}
