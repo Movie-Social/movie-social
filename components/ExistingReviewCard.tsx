86ff1c96abb4800c692843bbe182169d590462a8
Kendall McGree <mcgreekendall@gmail.com>
2023-07-09 17:36:57 -0500

    add styling to Review wrapper and component

--- components/ExistingReviewCard.tsx
+++ components/ExistingReviewCard.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 interface ExistingReviewCardProps {
   review: string;
   rating: number;
@@ -6,12 +7,20 @@ const ExistingReviewCard: React.FC<ExistingReviewCardProps> = ({
   rating,
   review,
 }) => {
-  console.log(rating, "rationg");
-  console.log(review, "review");
   return (
-    <>
-      <h1>hey</h1>
-    </>
+    <main className="border-2 border-yellow-500 m-2 w-[50%]">
+      <div className="flex flex-row">
+        <Image
+          alt="Movie Social logo"
+          className="ml-2"
+          src="/images/socialLogo.png"
+          width={50}
+          height={50}
+        />
+        <h2 className="ml-5">{review}</h2>
+      </div>
+      <h2>{rating}/5</h2>
+    </main>
   );
 };
 export default ExistingReviewCard;
