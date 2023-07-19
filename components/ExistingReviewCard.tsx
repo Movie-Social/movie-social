f24c5ce7661d87c35d10ece29fa01ef6a568b21d
Kendall McGree <mcgreekendall@gmail.com>
2023-07-19 17:23:03 -0500

    update styling of ExistingReviewCard component

--- components/ExistingReviewCard.tsx
+++ components/ExistingReviewCard.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { BiUserCircle } from "react-icons/bi";
 interface ExistingReviewCardProps {
   review: string;
   rating: number;
@@ -10,19 +11,24 @@ const ExistingReviewCard: React.FC<ExistingReviewCardProps> = ({
   name,
 }) => {
   return (
-    <main className="border-2 border-yellow-500 m-2 w-[50%]">
-      <div className="flex flex-row">
-        <Image
-          alt="Movie Social logo"
-          className="ml-2"
-          src="/images/newLogo.png"
-          width={50}
-          height={50}
-        />
-        <h2 className="ml-5">{review}</h2>
-      </div>
-      <h2>{rating}/5</h2>
-      <p>{name}</p>
+    <main>
+      <section className="border-2 border-yellow-500 p-2 m-2 flex flex-col items-center">
+        <div className="flex flex-row items-center content-center justify-center">
+          <Image
+            alt="Movie Social logo"
+            className="ml-2 rounded-full"
+            src="/images/newLogo.png"
+            width={40}
+            height={30}
+          />
+          <h2 className="ml-5">{review}</h2>
+        </div>
+        {rating === 0 ? <h2>Rating: N/A</h2> : <h2>Rating: {rating}/5</h2>}
+      </section>
+      <section className="flex flex-row justify-center">
+        <BiUserCircle className="text-white" color="white" size={40} />
+        <p className="text-yellow-300">{name}</p>
+      </section>
     </main>
   );
 };
