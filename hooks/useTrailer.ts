75a7faa1cd075e0c4a4b51493dd30673871edbd1
Kendall McGree <mcgreekendall@gmail.com>
2023-07-20 18:53:22 -0500

    fetch specific movie trailer dynamically and add conditional rendering since it does not work most of the time, bug

--- hooks/useTrailer.ts
+++ hooks/useTrailer.ts
@@ -2,12 +2,7 @@ import useSWR from "swr";
 import trailerFetcher from "@/lib/trailerFetcher";
 
 const useTrailer = (tmdbId?: string) => {
-  const { data, error, isLoading } = useSWR(
-    tmdbId
-      ? `https://api.themoviedb.org/3/movie/${tmdbId}?language=en-US&page=1`
-      : null,
-    trailerFetcher
-  );
+  const { data, error, isLoading } = useSWR(trailerFetcher);
 
   return { data, error, isLoading };
 };
