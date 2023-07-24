3e2dd9f49e7eb4d920dff473a93adf8e0947f112
Kendall McGree <mcgreekendall@gmail.com>
2023-07-24 18:53:57 -0500

    finish adding stlying to main MyProfile page for mobile

--- pages/myProfile.tsx
+++ pages/myProfile.tsx
@@ -31,7 +31,7 @@ const MyProfile = () => {
             </div>
           </div>
         </aside>
-        <section className="flex flex-col justify-around w-5/5 lg:w-3/5 border border-red-500">
+        <section className="flex flex-col justify-around w-5/5 lg:w-3/5 px-2">
           <div className="flex flex-row justify-between items-center">
             <h2 className="border-l-2 border-yellow-500 mb-2 mx-2 px-2 text-1xl lg:text-2xl font-bold">
               Favorites
@@ -59,15 +59,15 @@ const MyProfile = () => {
             <h2 className="border-l-2 border-yellow-500 mb-2 mx-2 px-2 text-1xl lg:text-2xl font-bold">
               Watchlist
             </h2>
-            <h2 className="text-yellow-400 cursor-pointer text-l lg:text-xl font-light">
+            {/* <h2 className="text-yellow-400 cursor-pointer text-l lg:text-xl font-light">
               View All
-            </h2>
+            </h2> */}
           </div>
           {watchlist?.length >= 1 ? (
             <ProfileList data={watchlist} />
           ) : (
             <div className="flex flex-col justify-center items-center content-center border-yellow-500 border-2 rounded-md mb-5 p-3">
-              <GrAddCircle size={50} />
+              <GrAddCircle className="text-white" size={50} />
               <h2>Your Watchlist is Empty</h2>
               <p>Add movies to keep track of what you want to watch</p>
               <button
@@ -82,19 +82,19 @@ const MyProfile = () => {
             <h2 className="border-l-2 border-yellow-500 mb-2 mx-2 px-2 text-1xl lg:text-2xl font-bold">
               My Reviews
             </h2>
-            <h2 className="text-yellow-400 cursor-pointer text-l lg:text-xl font-light">
+            {/* <h2 className="text-yellow-400 cursor-pointer text-l lg:text-xl font-light">
               View All
-            </h2>
+            </h2> */}
           </div>
           {reviews?.length >= 1 ? (
             <ReviewList data={reviews} />
           ) : (
-            <div className="flex flex-col justify-center items-center content-center border-yellow-500 border-2 rounded-md mb-5 p-3">
+            <div className="flex flex-col justify-center items-center content-around border-yellow-500 border-2 rounded-md mb-5 p-3">
               <GrAddCircle size={50} />
               <h2>No Reviews Yet</h2>
               <p className="text-center">
-                Rate movies you have seen using our 5 star scale to share your
-                reactions and opinions
+                Rate movies you have seen using to share your reactions and
+                opinions
               </p>
               <button
                 onClick={() => router.push("/")}
