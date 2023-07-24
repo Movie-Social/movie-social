d617b8edec25dc44b6d0c5e2e29bf5d0266aaccd
Kendall McGree <mcgreekendall@gmail.com>
2023-07-24 17:39:53 -0500

    update MovieList styling to match RestfulMovieDetails

--- components/MovieList.tsx
+++ components/MovieList.tsx
@@ -1,6 +1,7 @@
 import { isEmpty } from "lodash";
 import MovieCard from "./MovieCard";
 import { Slide } from "react-slideshow-image";
+import { BiSkipNext, BiSkipPrevious } from "react-icons/bi";
 interface MovieListProps {
   data: Record<string, any>[];
   title: string;
@@ -10,8 +11,8 @@ const MovieList: React.FC<MovieListProps> = ({ data, title }) => {
     return null;
   }
   return (
-    <main className="px-4 md:px-12 mt-2 space-y-4">
-      <section>
+    <main className="px-2 mt-2 space-y-4 flex justify-center">
+      <section className="flex flex-col content-center w-full">
         <p className="text-white text-md md:text-xl lg:text-2xl font-semibold mb-4">
           {title}
         </p>
@@ -19,8 +20,30 @@ const MovieList: React.FC<MovieListProps> = ({ data, title }) => {
           transitionDuration={1000}
           autoplay={false}
           slidesToScroll={3}
-          slidesToShow={5}
+          slidesToShow={4}
           indicators={true}
+          nextArrow={
+            <button
+              style={{
+                cursor: "pointer",
+                border: "0px",
+                opacity: "90%",
+              }}
+            >
+              <BiSkipNext size={40} className="text-yellow-300" />
+            </button>
+          }
+          prevArrow={
+            <button
+              style={{
+                cursor: "pointer",
+                border: "0px",
+                opacity: "90%",
+              }}
+            >
+              <BiSkipPrevious size={40} className="text-yellow-300" />
+            </button>
+          }
         >
           {title === "My Watchlist" &&
             data.map((movie) => <MovieCard key={movie.id} data={movie} />)}
