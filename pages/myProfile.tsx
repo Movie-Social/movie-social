1b6390d78828b5b34de22698c3e0f756170d7386
Kendall McGree <mcgreekendall@gmail.com>
2023-07-24 19:47:25 -0500

    clean code in MyProfile and ReviewListItem components

--- pages/myProfile.tsx
+++ pages/myProfile.tsx
@@ -1,13 +1,13 @@
-import useCurrentUser from "@/hooks/useCurrentUser";
-import useFavorites from "@/hooks/useFavorites";
-import useWatchlist from "@/hooks/useWatchlist";
-import useReviews from "@/hooks/useReviews";
-import ProfileList from "@/components/ProfileList";
-import { BiUserCircle } from "react-icons/bi";
-import { GrAddCircle } from "react-icons/gr";
 import { useRouter } from "next/router";
+import ProfileList from "@/components/ProfileList";
 import ReviewList from "@/components/ReviewList";
 import Navbar from "@/components/Navbar";
+import useCurrentUser from "@/hooks/useCurrentUser";
+import useFavorites from "@/hooks/useFavorites";
+import useReviews from "@/hooks/useReviews";
+import useWatchlist from "@/hooks/useWatchlist";
+import { BiUserCircle } from "react-icons/bi";
+import { GrAddCircle } from "react-icons/gr";
 
 const MyProfile = () => {
   const { data: currentUser } = useCurrentUser();
@@ -20,11 +20,11 @@ const MyProfile = () => {
       <Navbar />
       <main className="flex flex-col lg:flex-row justify-around w-[100vw] lg:w-[70vw] py-5 mt-10">
         <aside className="flex flex-col lg:justify-center lg:items-center content-center w-5/5 lg:w-1/5 h-1/5 p-2 mb-3">
-          <h2 className="border-l-2 border-yellow-500 mb-2 mx-2 px-2 text-1xl lg:text-2xl font-bold">
+          <h2 className="text-2xl lg:text-3xl font-bold mb-2 mx-2 px-2 border-l-2 border-yellow-300">
             Profile
           </h2>
           <div className="flex flex-row items-center">
-            <BiUserCircle size={50} />
+            <BiUserCircle size={35} />
             <div className="px-2">
               <h2>{currentUser?.name}</h2>
               <h2>{currentUser?.email}</h2>
@@ -33,7 +33,7 @@ const MyProfile = () => {
         </aside>
         <section className="flex flex-col justify-around w-5/5 lg:w-3/5 px-2">
           <div className="flex flex-row justify-between items-center">
-            <h2 className="border-l-2 border-yellow-500 mb-2 mx-2 px-2 text-1xl lg:text-2xl font-bold">
+            <h2 className="text-2xl lg:text-3xl font-bold mb-2 mx-2 px-2 border-l-2 border-yellow-300">
               Favorites
             </h2>
             {/* <h2 className="text-yellow-400 cursor-pointer text-l lg:text-xl font-light">
@@ -43,20 +43,20 @@ const MyProfile = () => {
           {faves?.length >= 1 ? (
             <ProfileList data={faves} />
           ) : (
-            <div className="flex flex-col justify-center items-center content-center border-yellow-500 border-2 rounded-md mb-5 p-3">
+            <div className="flex flex-col justify-center items-center content-center mb-5 p-3 border-yellow-300 border-2 rounded-md">
               <GrAddCircle className="text-white" size={50} />
               <h2>Your Favorites are Empty</h2>
               <p>Add movies to keep track of your favorites</p>
               <button
                 onClick={() => router.push("/")}
-                className="bg-yellow-500 rounded-md p-1 m-2"
+                className="p-1 m-2 bg-yellow-300 rounded-md"
               >
                 Browse Movies
               </button>
             </div>
           )}
           <div className="flex flex-row justify-between items-center">
-            <h2 className="border-l-2 border-yellow-500 mb-2 mx-2 px-2 text-1xl lg:text-2xl font-bold">
+            <h2 className="text-2xl lg:text-3xl font-bold mb-2 mx-2 px-2 border-l-2 border-yellow-300">
               Watchlist
             </h2>
             {/* <h2 className="text-yellow-400 cursor-pointer text-l lg:text-xl font-light">
@@ -66,20 +66,20 @@ const MyProfile = () => {
           {watchlist?.length >= 1 ? (
             <ProfileList data={watchlist} />
           ) : (
-            <div className="flex flex-col justify-center items-center content-center border-yellow-500 border-2 rounded-md mb-5 p-3">
+            <div className="flex flex-col justify-center items-center content-center mb-5 p-3 border-yellow-300 border-2 rounded-md">
               <GrAddCircle className="text-white" size={50} />
               <h2>Your Watchlist is Empty</h2>
               <p>Add movies to keep track of what you want to watch</p>
               <button
                 onClick={() => router.push("/")}
-                className="bg-yellow-500 rounded-md p-1 m-2"
+                className="p-1 m-2 bg-yellow-300 rounded-md"
               >
                 Browse Movies
               </button>
             </div>
           )}
           <div className="flex flex-row justify-between items-center">
-            <h2 className="border-l-2 border-yellow-500 mb-2 mx-2 px-2 text-1xl lg:text-2xl font-bold">
+            <h2 className="text-2xl lg:text-3xl font-bold mb-2 mx-2 px-2 border-l-2 border-yellow-300">
               My Reviews
             </h2>
             {/* <h2 className="text-yellow-400 cursor-pointer text-l lg:text-xl font-light">
@@ -89,7 +89,7 @@ const MyProfile = () => {
           {reviews?.length >= 1 ? (
             <ReviewList data={reviews} />
           ) : (
-            <div className="flex flex-col justify-center items-center content-around border-yellow-500 border-2 rounded-md mb-5 p-3">
+            <div className="flex flex-col justify-center items-center content-around mb-5 p-3 border-yellow-300 border-2 rounded-md">
               <GrAddCircle size={50} />
               <h2>No Reviews Yet</h2>
               <p className="text-center">
@@ -98,7 +98,7 @@ const MyProfile = () => {
               </p>
               <button
                 onClick={() => router.push("/")}
-                className="bg-yellow-500 rounded-md p-1 m-2"
+                className="p-1 m-2 bg-yellow-300 rounded-md"
               >
                 Browse Movies
               </button>
