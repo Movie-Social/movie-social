4a1c9d6e3d699e07ff437c0810806229753b683e
Kendall McGree <mcgreekendall@gmail.com>
2023-07-26 19:09:50 -0500

    update styling for large screen 404 styling

--- pages/404.js
+++ pages/404.js
@@ -2,26 +2,29 @@ import Image from "next/image";
 import movie404 from "../public/images/movie404.gif";
 const ErrorPage = () => {
   return (
-    <main className="flex flex-col justify-center w-full h-full text-white border border-red-500">
-      <h2 className="text-center">
-        If you have reached this page you caught the developer (Kendall McGree)
+    <main className="flex flex-col justify-center w-full h-full text-white">
+      <h2 className="text-center lg:text-3xl">
+        If you have reached this page you caught the developer{" "}
+        <span className="text-yellow-300 font-light">
+          {"(Kendall McGree) "}
+        </span>
         off his game 🤦🏾‍♂️
       </h2>
       <Image
         src={movie404}
         alt="Gif of John Travolta looking for the internet connection"
-        className="w-4/5 self-center my-4"
+        className="w-4/5 lg:w-3/5 self-center my-4 rounded-lg"
       />
-      <h1 className="text-center text-2xl lg:text-3xl text-yellow-300 font-semibold">
+      <h1 className="text-center text-xl lg:text-3xl text-yellow-300 font-semibold">
         Two Options
       </h1>
-      <h2 className="text-center">
+      <h2 className="text-center lg:text-2xl">
         <span className="text-yellow-300 font-light">{"A.)"}</span> Help me by
-        not doing whatever you did to get here. This method will help me not
-        look bad on my own app
+        not doing whatever you did to get here again. This method will help me
+        not look bad on my own app 😂
       </h2>
       <br></br>
-      <h2 className="text-center">
+      <h2 className="text-center lg:text-2xl">
         <span className="text-yellow-300 font-light">{"B.)"}</span> Help
         troubleshoot this puppy with me....
       </h2>
