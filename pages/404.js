ffc49aabce4a51befd2cdc0c41e4d8fd4d7013ad
Kendall McGree <mcgreekendall@gmail.com>
2023-07-26 19:04:23 -0500

    update styling for mobile 404 styling

--- pages/404.js
+++ pages/404.js
@@ -2,19 +2,29 @@ import Image from "next/image";
 import movie404 from "../public/images/movie404.gif";
 const ErrorPage = () => {
   return (
-    <main className="text-white">
-      <h2>
-        If you have reached this page you caught the developer slipping 🤦🏾‍♂️
+    <main className="flex flex-col justify-center w-full h-full text-white border border-red-500">
+      <h2 className="text-center">
+        If you have reached this page you caught the developer (Kendall McGree)
+        off his game 🤦🏾‍♂️
       </h2>
       <Image
         src={movie404}
         alt="Gif of John Travolta looking for the internet connection"
+        className="w-4/5 self-center my-4"
       />
-      <h2>
-        Either help by not doing whatever you did to get here thus not making me
+      <h1 className="text-center text-2xl lg:text-3xl text-yellow-300 font-semibold">
+        Two Options
+      </h1>
+      <h2 className="text-center">
+        <span className="text-yellow-300 font-light">{"A.)"}</span> Help me by
+        not doing whatever you did to get here. This method will help me not
         look bad on my own app
       </h2>
-      <h2>Or help but troubleshooting this puppy for me #celebShot</h2>
+      <br></br>
+      <h2 className="text-center">
+        <span className="text-yellow-300 font-light">{"B.)"}</span> Help
+        troubleshoot this puppy with me....
+      </h2>
     </main>
   );
 };
