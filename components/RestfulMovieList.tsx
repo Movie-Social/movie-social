e24cb0c5127eca7c4b2abf260f342de2e615b438
Kendall McGree <mcgreekendall@gmail.com>
2023-07-28 17:47:26 -0500

    update RestfulMovieList component to have styled breakpoint for laptop range screen sizes

--- components/RestfulMovieList.tsx
+++ components/RestfulMovieList.tsx
@@ -42,9 +42,9 @@ const RestfulMovieList: React.FC<RestfulMovieListProps> = ({ title }) => {
   }
 
   return (
-    <main className="px-2 mt-2 space-y-4 flex justify-center">
+    <main className="px-2 mt-4 space-y-5 md:space-y-7 flex justify-center ">
       <section className="flex flex-col content-center w-full">
-        <h2 className="text-white text-md md:text-xl lg:text-2xl font-semibold mb-4">
+        <h2 className="text-white text-md md:text-xl lg:text-2xl font-semibold md:mx-2 my-4">
           {title}
         </h2>
         <Slide
