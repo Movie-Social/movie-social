8cfabbb7b165aa274802e2679767f44625babfa0
Kendall McGree <mcgreekendall@gmail.com>
2023-07-29 14:16:09 -0500

    add final adjustments and corrections to myprofile page

--- components/ProfileList.tsx
+++ components/ProfileList.tsx
@@ -10,7 +10,7 @@ const ProfileList: React.FC<ProfileListProps> = ({ data }) => {
   }
   return (
     <main className="w-full flex justify-center">
-      <section className="grid grid-cols-3 justify-center items-center content-center grid-col-g md:w-5/6 md:self-center mb-5 py-5 md:border-yellow-300 md:border-2 md:rounded-md max-[767px]:gap-2 md:gap-y-10">
+      <section className="grid grid-cols-3 md:w-5/6 md:self-center mb-5 py-5 md:border-white md:border-opacity-70 md:border md:rounded-md max-[767px]:gap-2 md:gap-y-10">
         {data.map((movie) => {
           return <ProfileListItem key={movie.id} movieId={movie.id} />;
         })}
--- components/ReviewList.tsx
+++ components/ReviewList.tsx
@@ -10,7 +10,7 @@ const ReviewList: React.FC<ReviewListProps> = ({ data }) => {
   }
   return (
     <main className="w-full flex justify-center">
-      <section className="flex flex-col justify-around md:w-5/6 my-3 md:border-yellow-300 md:border-2 md:rounded-md">
+      <section className="flex flex-col justify-around md:w-5/6 my-3 md:border-white md:border-opacity-70 md:border md:rounded-md">
         {data.map((movie) => {
           return <ReviewListItem key={movie.id} data={movie} />;
         })}
