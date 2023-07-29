c515de7bf187b1c7478373078db4b244a3154e8f
Kendall McGree <mcgreekendall@gmail.com>
2023-07-28 20:45:25 -0500

    update myProfile's ReviewList components' styling for laptop range breakpoints

--- components/ReviewList.tsx
+++ components/ReviewList.tsx
@@ -9,12 +9,12 @@ const ReviewList: React.FC<ReviewListProps> = ({ data }) => {
     return null;
   }
   return (
-    <main>
-      <div className="flex flex-col justify-around my-3 border-yellow-300 border">
+    <main className="w-full flex justify-center">
+      <section className="flex flex-col justify-around md:w-5/6 my-3 md:border-yellow-300 md:border-2 md:rounded-md">
         {data.map((movie) => {
           return <ReviewListItem key={movie.id} data={movie} />;
         })}
-      </div>
+      </section>
     </main>
   );
 };
