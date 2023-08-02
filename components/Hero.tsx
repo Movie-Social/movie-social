f6bc478b12d825d1f0e8b69d8c0f4d418c4d64be
Kendall McGree <mcgreekendall@gmail.com>
2023-08-01 21:26:38 -0500

    update hero endpoint and component to render random movies from the be a oppposed to hardcode local data

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,4 +1,4 @@
-import { Key } from "react";
+import { Key, useEffect } from "react";
 import { BsArrowLeft } from "react-icons/bs";
 import { BsArrowRight } from "react-icons/bs";
 import { Slide } from "react-slideshow-image";
@@ -12,154 +12,15 @@ import RestfulMovieList from "./RestfulMovieList";
 import useAllHero from "@/hooks/useAllHero";
 
 interface ShuffleArgs {
-  image: string;
+  poster: string;
   caption: string;
+  title: string;
 }
 
 const Hero = () => {
   const allHero = useAllHero();
-  const images = [
-    {
-      image: "/images/hero/IMG_8084.jpg",
-      caption: "Scene from the movie Deadpool",
-    },
-    {
-      image: "/images/hero/IMG_8085.jpg",
-      caption: "Scene from the movie Training Day",
-    },
-    {
-      image: "/images/hero/IMG_8086.jpg",
-      caption: "Scene from the movie Big Lebowski",
-    },
-    {
-      image: "/images/hero/IMG_8087.jpg",
-      caption: "Scene from the movie Moonlight",
-    },
-    {
-      image: "/images/hero/IMG_8088.jpg",
-      caption: "Scene from the movie Big Daddy",
-    },
-    {
-      image: "/images/hero/IMG_8089.jpg",
-      caption: "Scene from the movie Get Out",
-    },
-    {
-      image: "/images/hero/IMG_8090.jpg",
-      caption: "Scene from the movie Guardians of The Galaxy",
-    },
-    {
-      image: "/images/hero/IMG_8091.jpg",
-      caption: "Scene from the movie Sicario",
-    },
-    {
-      image: "/images/hero/IMG_8092.jpg",
-      caption: "Scene from the movie Precious",
-    },
-    {
-      image: "/images/hero/IMG_8093.jpg",
-      caption: "Scene from the movie V for Vendetta",
-    },
-    {
-      image: "/images/hero/IMG_8094.jpg",
-      caption: "Scene from the movie The Other Boleyn Girl ",
-    },
-    {
-      image: "/images/hero/IMG_8095.jpg",
-      caption: "Scene from the movie Half Baked",
-    },
-    {
-      image: "/images/hero/IMG_8096.jpg",
-      caption: "Scene from the movie Super Troopers",
-    },
-    {
-      image: "/images/hero/IMG_8097.jpg",
-      caption: "Scene from the movie Road Trip",
-    },
-    {
-      image: "/images/hero/IMG_8098.jpg",
-      caption: "Scene from the movie Memento",
-    },
-    {
-      image: "/images/hero/IMG_8099.jpg",
-      caption: "Scene from the movie King's Speech",
-    },
-    {
-      image: "/images/hero/IMG_8100.jpg",
-      caption: "Scene from the movie Primal Fear",
-    },
-    {
-      image: "/images/hero/IMG_8101.jpg",
-      caption: "Scene from the movie Pulp Fiction",
-    },
-    {
-      image: "/images/hero/IMG_8102.jpg",
-      caption: "Scene from the movie Focus",
-    },
-    {
-      image: "/images/hero/IMG_8103.jpg",
-      caption: "Scene from the movie Chappie",
-    },
-    {
-      image: "/images/hero/IMG_8104.jpg",
-      caption: "Scene from the movie Love and Basketball",
-    },
-    {
-      image: "/images/hero/IMG_8105.jpg",
-      caption: "Scene from the movie Next Day Air ",
-    },
-    {
-      image: "/images/hero/IMG_8106.jpg",
-      caption: "Scene from the movie Baby Boy",
-    },
-    {
-      image: "/images/hero/IMG_8107.jpg",
-      caption: "Scene from the movie No Country for Old Men",
-    },
-    {
-      image: "/images/hero/IMG_8108.jpg",
-      caption: "Scene from the movie Poetic Justice",
-    },
-    {
-      image: "/images/hero/IMG_8109.jpg",
-      caption: "Scene from the movie Juice",
-    },
-    {
-      image: "/images/hero/IMG_8110.jpg",
-      caption: "Scene from the movie The Wood",
-    },
-    {
-      image: "/images/hero/IMG_8111.jpg",
-      caption: "Scene from the movie A Thin Line Between Love and Hate",
-    },
-    {
-      image: "/images/hero/IMG_8112.jpg",
-      caption: "Scene from the movie Forrest Gump",
-    },
-    {
-      image: "/images/hero/IMG_8113.jpg",
-      caption: "Scene from the movie Dark Night",
-    },
-    {
-      image: "/images/hero/IMG_8114.jpg",
-      caption: "Scene from the movie InterStellar",
-    },
-    {
-      image: "/images/hero/IMG_8115.jpg",
-      caption: "Scene from the movie Wolf Of Wall Street",
-    },
-    {
-      image: "/images/hero/IMG_8116.jpg",
-      caption: "Scene from the movie Lord of the Rings",
-    },
-    {
-      image: "/images/hero/IMG_8117.jpg",
-      caption: "Scene from the movie Braveheart",
-    },
-    {
-      image: "/images/hero/IMG_8118.jpg",
-      caption: "Scene from the movie Robinhood Men In Tights",
-    },
-  ];
+
+  const heroOptions = allHero?.data;
 
   const shuffle = (array: ShuffleArgs[]) => {
     let currentIndex = array.length,
@@ -177,9 +38,7 @@ const Hero = () => {
 
     return array;
   };
-
-  const shuffled = shuffle(images);
-  console.log("steve", allHero.data);
+  const shuffled = shuffle(heroOptions);
   return (
     <main className="lg:w-full lg:h-[100vh] lg:rounded-lg self-center opacity-50 border mb-10 border-blue-700">
       <Slide
@@ -211,7 +70,7 @@ const Hero = () => {
         {shuffled.map((slideImage, index) => (
           <div className="relative h-56 lg:h-[100vh] w-full" key={index}>
             <Image
-              src={slideImage.image}
+              src={slideImage.poster}
               fill
               priority
               alt={slideImage.caption}
