b36bb11eb9b93c604b0fd349259998bd23426511
Kendall McGree <mcgreekendall@gmail.com>
2023-08-04 17:22:24 -0500

    update MovieCard styling for new design

--- components/RestfulMovieList.tsx
+++ components/RestfulMovieList.tsx
@@ -43,7 +43,7 @@ const RestfulMovieList: React.FC<RestfulMovieListProps> = ({ title }) => {
 
   return (
     <main className="flex justify-center w-full self-center px-2 mt-4 space-y-5 md:space-y-7">
-      <section className="flex flex-col content-center w-full">
+      <section className="flex flex-col w-full">
         <h2 className="text-white text-md md:text-xl lg:text-2xl font-semibold md:mx-2 my-4">
           {title}
         </h2>
@@ -89,7 +89,7 @@ const RestfulMovieList: React.FC<RestfulMovieListProps> = ({ title }) => {
             transitionDuration={1000}
             autoplay={false}
             slidesToScroll={3}
-            slidesToShow={6}
+            slidesToShow={5}
             indicators={true}
             nextArrow={
               <button
