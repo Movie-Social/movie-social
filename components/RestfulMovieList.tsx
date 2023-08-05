b97a18b1df47628ee73d85fc4601232185ca2ba1
Kendall McGree <mcgreekendall@gmail.com>
2023-08-05 15:16:04 -0500

    complete RestfulMovieCard component styling

--- components/RestfulMovieList.tsx
+++ components/RestfulMovieList.tsx
@@ -44,7 +44,7 @@ const RestfulMovieList: React.FC<RestfulMovieListProps> = ({ title }) => {
   return (
     <main className="flex justify-center w-full self-center px-2 mt-4 space-y-5 md:space-y-7">
       <section className="flex flex-col w-full">
-        <h2 className="text-white text-md md:text-xl lg:text-2xl font-semibold md:mx-2 my-4">
+        <h2 className="text-white text-md md:text-xl lg:text-2xl font-semibold md:mx-20 my-4">
           {title}
         </h2>
         {/* Small to medium slideshow */}
