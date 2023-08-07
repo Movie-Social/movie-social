43fc30e63a67469b47171c63a447672f9366b036
Kendall McGree <mcgreekendall@gmail.com>
2023-08-07 08:12:37 -0500

    fox bug that wouldnt let review restful movies pictures populate

--- components/ReviewListItem.tsx
+++ components/ReviewListItem.tsx
@@ -1,15 +1,12 @@
 import { useRouter } from "next/router";
 import Image from "next/image";
 import useMovieByTitle from "@/hooks/useMovieByTitle";
-import loady from "../public/images/imgLoad.gif";
 interface ReviewListItemProps {
   data: Record<string, any>;
 }
-
 const ReviewListItem: React.FC<ReviewListItemProps> = ({ data }) => {
   const router = useRouter();
   const movieMovie = useMovieByTitle(data.title);
-  const isLoading = useMovieByTitle(data.title);
   return (
     <main className="flex justify-center">
       <section className="w-[95%] my-3 p-3 border-b-2 border-yellow-300 border-opacity-30">
@@ -20,25 +17,16 @@ const ReviewListItem: React.FC<ReviewListItemProps> = ({ data }) => {
           {data?.title}
         </h2>
         <div className="flex flex-row">
-          {isLoading.isLoading ? (
-            <Image
-              src={loady}
-              alt="Loading gif animation"
-              width={130}
-              height={50}
-            />
-          ) : (
-            <Image
-              priority
-              className="cursor-pointer transition hover:opacity-70"
-              onClick={() => router.push(`/movie/${movieMovie.data}`)}
-              src={data.poster}
-              alt={`${data.title}'s official 
+          <Image
+            priority
+            className="cursor-pointer transition hover:opacity-70"
+            onClick={() => router.push(`/movie/${movieMovie.data}`)}
+            src={data.poster}
+            alt={`${data.title}'s official 
           movie poster`}
-              width={130}
-              height={50}
-            />
-          )}
+            width={130}
+            height={50}
+          />
           <div className="flex flex-col justify-evenly">
             {data.review ? (
               <p className="mx-3">{data.review}</p>
