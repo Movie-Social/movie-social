520bbdc3b257b4e607f3ebde4fe3ae6f138f37ff
Kendall McGree <mcgreekendall@gmail.com>
2023-08-12 12:44:50 -0500

    update myProfile styling list for large breakpoint

--- components/ProfileList.tsx
+++ components/ProfileList.tsx
@@ -10,8 +10,8 @@ const ProfileList: React.FC<ProfileListProps> = ({ data }) => {
   }
 
   return (
-    <main className="w-full flex justify-center">
-      <section className="grid grid-cols-3 md:w-5/6 md:self-center my-5 px-2 py-5 border-white border-opacity-70 border rounded-md max-[767px]:gap-2 md:gap-y-10">
+    <main className="w-full ">
+      <section className="grid grid-cols-3 md:w-5/6 lg:w-full md:self-center my-5 px-2 py-5 border-white border-opacity-70 border rounded-md max-[767px]:gap-2 md:gap-y-10">
         {data.map((movie) => {
           return (
             <ProfileListItem key={movie.id} movieId={movie.id} faves={movie} />
