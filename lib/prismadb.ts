7d053dde85f0e2c5b05ca0819ab5028263eda298
Kendall McGree <mcgreekendall@gmail.com>
2023-08-18 16:45:53 -0500

    add production variant to prismadb

--- lib/prismadb.ts
+++ lib/prismadb.ts
@@ -3,6 +3,6 @@ import { PrismaClient } from "@prisma/client";
 //saving prisma client in a global file since global files are not affected by hot reloading
 // I will not need to do this in production
 const client = global.prismadb || new PrismaClient();
-if (process.env.NODE_ENV === "production") global.prismadb = client;
+if (process.env.NODE_ENV !== "production") global.prismadb = client;
 
 export default client;
