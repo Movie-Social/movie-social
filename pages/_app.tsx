4b8ec7357fb688ff29bb8aae85545e089bfd60e6
Kendall McGree <mcgreekendall@gmail.com>
2023-08-18 15:54:03 -0500

    add session to app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,16 @@
-import "@/styles/globals.css";
+import React from "react";
+import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
+import "@/styles/globals.css";
 import "normalize.css/normalize.css";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />;
+    </SessionProvider>
+  );
 }
