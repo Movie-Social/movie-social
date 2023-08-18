599f069ab3b07a3739bc8b304097591c2ed418ab
Kendall McGree <mcgreekendall@gmail.com>
2023-08-18 16:10:29 -0500

    remove session

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,23 @@
 import React from "react";
-import { SessionProvider } from "next-auth/react";
-import type { AppProps } from "next/app";
 import "@/styles/globals.css";
+import type { AppProps } from "next/app";
 import "normalize.css/normalize.css";
 
-export default function App({
-  Component,
-  pageProps: { session, ...pageProps },
-}: AppProps) {
-  return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />;
-    </SessionProvider>
-  );
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
 }
+// import { SessionProvider } from "next-auth/react";
+// import type { AppProps } from "next/app";
+// import "@/styles/globals.css";
+// import "normalize.css/normalize.css";
+
+// export default function App({
+//   Component,
+//   pageProps: { session, ...pageProps },
+// }: AppProps) {
+//   return (
+//     <SessionProvider session={session}>
+//       <Component {...pageProps} />;
+//     </SessionProvider>
+//   );
+// }
