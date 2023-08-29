54fd35660516f504c24957f56aa34103c8253cb8
Kendall McGree <mcgreekendall@gmail.com>
2023-08-28 20:02:29 -0500

    update Hero component to include slideshow of data fetched internally without hydration errors

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,51 +1,63 @@
-import { useEffect, useState } from "react";
 import Image from "next/image";
-import useAllHero from "@/hooks/useAllHero";
 import "react-slideshow-image/dist/styles.css";
 import { useRouter } from "next/router";
-import tmdbMovieFetcher from "@/lib/tmdbMovieFetcher";
+import { Slide } from "react-slideshow-image";
+import { images } from "@/lib/heroData";
+import { useEffect, useState } from "react";
 
-interface Movie {
+interface ShuffleArgs {
   id: number;
+  image: string;
+  caption: string;
 }
 
 const Hero = () => {
-  const allHero = useAllHero();
   const router = useRouter();
-  const heroOptions = allHero?.data;
-  const [movie, setMovie] = useState<Movie>();
+  const [shuffled, setShuffled] = useState<ShuffleArgs[]>();
+
+  const shuffle = (array: ShuffleArgs[]) => {
+    let currentIndex = array.length,
+      randomIndex;
+    while (currentIndex != 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex],
+        array[currentIndex],
+      ];
+    }
+    return array;
+  };
 
   useEffect(() => {
-    const fetchTMDBLists = async () => {
-      const fetchedMovie = await tmdbMovieFetcher(heroOptions?.title);
-      setMovie(fetchedMovie?.results[0]);
-    };
-    fetchTMDBLists();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [heroOptions]);
+    const heroImages = shuffle(images);
+    setShuffled(heroImages);
+  }, []);
 
   return (
     <main className="relative lg:w-full lg:h-[80vh] self-center opacity-50">
-      <div className="relative h-56 lg:h-[100vh] w-full">
-        <Image
-          onClick={() => router.push(`/movie/tmdb/${movie?.id}`)}
-          src={heroOptions?.poster}
-          fill
-          priority
-          alt={heroOptions?.caption}
-          className="cursor-pointer"
-        />
-        <div className="absolute inset-x-1/4 top-7 text-center z-10 md:text-5xl text-2xl bold text-white">
-          <button>
-            <h2
-              onClick={() => router.push(`/movie/tmdb/${movie?.id}`)}
-              className="bg-black bg-clip-text cursor-pointer"
-            >
-              {heroOptions?.caption.split("Scene from the movie ")[1]}
-            </h2>
-          </button>
-        </div>
-      </div>
+      <Slide>
+        {shuffled?.map((slideImage, index) => (
+          <div className="relative h-56 lg:h-[100vh] w-full" key={index}>
+            <Image
+              src={slideImage.image}
+              fill
+              priority
+              alt={slideImage.caption}
+            />
+            <div className="absolute inset-x-1/4 top-7 text-center z-10 md:text-5xl text-2xl bold text-white">
+              <button>
+                <h2
+                  onClick={() => router.push(`/movie/tmdb/${slideImage.id}`)}
+                  className="bg-black bg-clip-text cursor-pointer"
+                >
+                  {slideImage.caption.split("Scene from the movie ")[1]}
+                </h2>
+              </button>
+            </div>
+          </div>
+        ))}
+      </Slide>
     </main>
   );
 };
