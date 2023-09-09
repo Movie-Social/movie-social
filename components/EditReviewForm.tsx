cd35b09fad5dd313a969161b01f3501a89554879
Kendall McGree <mcgreekendall@gmail.com>
2023-09-09 16:07:41 -0500

    create EditReviewform component and add conditional render to button for toggling between updating

---
+++ components/EditReviewForm.tsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import { MdOutlineCancel } from "react-icons/md";
+
+interface ExistingReviewFormProps {
+  review: string;
+  rating: number;
+  name: string;
+}
+const ExistingReviewForm: React.FC<ExistingReviewFormProps> = ({
+  rating,
+  review,
+  name,
+}) => {
+  const [updatedReview, setUpdatedReview] = useState<string>("");
+  const [updating, setUpdating] = useState<boolean>(false);
+
+  const handleChange = (event: any) => {
+    const { value } = event.target;
+    setUpdatedReview(value);
+  };
+
+  return (
+    <main className="flex flex-col items-center w-full h-full">
+      <section className="flex flex-col items-center justify-center md:w-full h-full p-2 m-2 md:m-5 border-2 border-yellow-300 rounded-md">
+        <MdOutlineCancel className="text-yellow-300 self-center" size={40} />
+        {
+          <textarea
+            placeholder="Update your review"
+            value={updatedReview}
+            onChange={handleChange}
+            className="w-full h-full text-center overflow-hidden md:text-xl lg:text-xl text-white bg-transparent my-3 rounded-md"
+          />
+        }
+      </section>
+    </main>
+  );
+};
+export default ExistingReviewForm;
--- components/ExistingReviewCard.tsx
+++ components/ExistingReviewCard.tsx
@@ -2,6 +2,9 @@ import Image from "next/image";
 import { BiUserCircle } from "react-icons/bi";
 import useCurrentUser from "@/hooks/useCurrentUser";
 import EditReviewButton from "./EditReviewButton";
+import { useState } from "react";
+import EditReviewForm from "./EditReviewForm";
+
 interface ExistingReviewCardProps {
   review: string;
   rating: number;
@@ -13,32 +16,42 @@ const ExistingReviewCard: React.FC<ExistingReviewCardProps> = ({
   name,
 }) => {
   const currentUser = useCurrentUser();
+  const [updating, setUpdating] = useState<boolean>(false);
+
+  const updateReview = () => {
+    setUpdating(true);
+  };
 
   return (
     <main className="flex flex-col items-center">
-      <section className="flex flex-col items-center justify-center md:w-full h-full p-2 m-2 md:m-5 border-2 border-yellow-300 rounded-md">
-        {/* onClick={() =>
-            console.log(rating, review, name, currentUser.data.name)
-          } */}
-        <EditReviewButton review={review} usersName={name} />
-        <div className="flex flex-row justify-center items-center content-center">
-          <Image
-            alt="Movie Social logo"
-            className="ml-2 rounded-full"
-            src="/images/newLogo.png"
-            width={40}
-            height={30}
-          />
-          <h2 className="md:text-lg lg:text-xl ml-5">{review}</h2>
-        </div>
-        {rating === 0 ? (
-          <h2 className="md:text-lg lg:text-xl font-semibold">Rating: N/A</h2>
-        ) : (
-          <h2 className="md:text-lg lg:text-xl font-semibold mt-4">
-            Rating: {rating}/5
-          </h2>
-        )}
-      </section>
+      {!updating ? (
+        <section className="flex flex-col items-center justify-center md:w-full h-full p-2 m-2 md:m-5 border-2 border-yellow-300 rounded-md">
+          <div className="cursor-pointer" onClick={updateReview}>
+            {currentUser?.data?.name === name ? (
+              <EditReviewButton review={review} usersName={name} />
+            ) : null}
+          </div>
+          <div className="flex flex-row justify-center items-center content-center">
+            <Image
+              alt="Movie Social logo"
+              className="ml-2 rounded-full"
+              src="/images/newLogo.png"
+              width={40}
+              height={30}
+            />
+            <h2 className="md:text-lg lg:text-xl ml-5">{review}</h2>
+          </div>
+          {rating === 0 ? (
+            <h2 className="md:text-lg lg:text-xl font-semibold">Rating: N/A</h2>
+          ) : (
+            <h2 className="md:text-lg lg:text-xl font-semibold mt-4">
+              Rating: {rating}/5
+            </h2>
+          )}
+        </section>
+      ) : (
+        <EditReviewForm review={review} rating={rating} name={name} />
+      )}
       <section className="flex flex-row justify-center content-center self-center mb-5 w-full">
         <BiUserCircle className="text-white mx-2" size={30} />
         <p className="text-yellow-300 md:text-xl lg:text-2xl">{name}</p>
