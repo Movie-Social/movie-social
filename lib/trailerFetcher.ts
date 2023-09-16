ab4f68dc5dd442401e04802505bbf4ac1d637de0
Kendall McGree <mcgreekendall@gmail.com>
2023-09-16 12:54:31 -0500

    fix leak

--- lib/trailerFetcher.ts
+++ lib/trailerFetcher.ts
@@ -1,11 +1,14 @@
 const trailerFetcher = async (tmdbId: string) => {
+  if (!tmdbId) {
+    console.clear();
+  }
   try {
     const response = await fetch(
       `https://api.themoviedb.org/3/movie/${tmdbId}/videos?api_key=${process.env.NEXT_PUBLIC_TMDB}`
     );
     return response.json();
   } catch (error) {
-    console.log(error);
+    console.clear();
   }
 };
 export default trailerFetcher;
